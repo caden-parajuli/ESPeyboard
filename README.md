aa681c675a0341b1b34c31705ac4cd926805d71d
Caden Parajuli <caden.parajuli@member.fsf.org>
2025-03-20 20:36:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# ESPeyboard: USB keyboard relay
+
+Have you ever needed a USB keyboard and just didn't have one?
+This program allows you to use your laptop as a USB keyboard by sending keystrokes over Bluetooth to an ESP32-S3 which acts as a USB HID device to relay the keystrokes.
---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        nixpkgs-esp-dev = builtins.fetchGit {
+          url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
+          rev = "31ee58005f43e93a6264e3667c9bf5c31b368733";
+        };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ];
+        };
+      in
+      with pkgs;
+      {
+        devShell = mkShell {
+          buildInputs = with pkgs; [
+            # (esp-idf-full.override {
+            #   rev = "release/v5.4";
+            #   sha256 = "sha256-308d0gaHH9WiRawiPC3TKne6T+P1AZUA25OXO53WFtc=";
+            # })
+            esp32s3-idf
+            python312Packages.psutil
+          ];
+        };
+      }
+    );
+}
